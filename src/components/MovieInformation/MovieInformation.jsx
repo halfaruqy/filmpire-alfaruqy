24566693cb356b29a6a0bb421ebd40a3810a5686
Hilmi Alfaruqy <halfaruqy@gmail.com>
2024-03-24 08:34:43 +0700

    adding folder structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  console.log('Movie');
+
+  return <div>MovieInformation</div>;
+};
+
+export default MovieInformation;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
